d75760d0b2defe15d857cdac57c7738616d56a16
Peter Williams <pwil3058@bigpond.net.au>
2024-04-22 11:10:54 +1000

    Don't include eol at end of lines

--- diff_lib/src/lines.rs
+++ diff_lib/src/lines.rs
@@ -1,6 +1,5 @@
 // Copyright 2024 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
 
-use std::collections::HashMap;
 use std::ops::RangeBounds;
 
 use crypto_hash;
@@ -18,43 +17,19 @@ impl MapKey for str {
     }
 }
 
-#[derive(Debug, Default)]
-pub struct LineIndices(HashMap<Vec<u8>, Vec<usize>>);
-
-impl LineIndices {
-    fn add(&mut self, line: &str, index: usize) {
-        let key = line.map_key();
-        if let Some(vec) = self.0.get_mut(&key) {
-            vec.push(index);
-        } else {
-            self.0.insert(key, vec![index]);
-        }
-    }
-
-    pub fn get(&self, line: &str) -> Option<&Vec<usize>> {
-        self.0.get(&line.map_key())
-    }
-}
-
 pub trait BasicLines: Len + Default {
+    fn has_final_eol(&self) -> bool;
+    fn eol(&self) -> &str;
     fn lines(&self, range_bounds: impl RangeBounds<usize>) -> impl Iterator<Item = &str>;
     fn lines_reversed(&self, range_bounds: impl RangeBounds<usize>) -> impl Iterator<Item = &str>;
-}
-
-pub trait DiffInputLines: BasicLines {
-    fn get_line_indices(&self) -> LineIndices {
-        let mut line_indices = LineIndices::default();
-        for (i, item) in self.lines(..).enumerate() {
-            line_indices.add(item, i);
-        }
-        line_indices
-    }
 }
 
 #[derive(Debug, Default)]
 pub struct LazyLines {
     text: String,
     length: usize,
+    eol: &'static str,
+    has_final_eol: bool,
 }
 
 impl Len for LazyLines {
@@ -64,28 +39,44 @@ impl Len for LazyLines {
 }
 
 impl BasicLines for LazyLines {
+    fn has_final_eol(&self) -> bool {
+        self.has_final_eol
+    }
+
+    fn eol(&self) -> &str {
+        self.eol
+    }
+
     fn lines(&self, range_bounds: impl RangeBounds<usize>) -> impl Iterator<Item = &str> {
         let range = Range::from(range_bounds);
-        self.text
-            .split_inclusive("\n")
-            .skip(range.start())
-            .take(range.len())
+        self.text.lines().skip(range.start()).take(range.len())
     }
 
     fn lines_reversed(&self, range_bounds: impl RangeBounds<usize>) -> impl Iterator<Item = &str> {
         let range = Range::from(range_bounds);
-        let iter = self.text.split_inclusive('\n').rev();
-        iter.skip(self.length - range.end().min(self.length))
+        self.text
+            .lines()
+            .rev()
+            .skip(self.length - range.end().min(self.length))
             .take(range.end().min(self.length) - range.start())
     }
 }
 
-impl DiffInputLines for LazyLines {}
-
 impl From<String> for LazyLines {
     fn from(text: String) -> Self {
         let length = text.split_inclusive("\n").count();
-        Self { text, length }
+        let has_final_eol = text.ends_with("\n");
+        let eol = if let Some(_) = text.find("\r\n") {
+            "\r\n"
+        } else {
+            "\n"
+        };
+        Self {
+            text,
+            length,
+            has_final_eol,
+            eol,
+        }
     }
 }
 
@@ -98,33 +89,76 @@ impl From<&str> for LazyLines {
 #[cfg(test)]
 pub mod test_lines {
     use super::*;
+
     #[test]
     fn lazy_lines() {
         let lazy_lines: LazyLines = String::from("a\nb\nc\nd\n").into();
+        assert!(lazy_lines.has_final_eol());
+        assert_eq!(lazy_lines.eol(), "\n");
         assert_eq!(lazy_lines.len(), 4);
         assert_eq!(
-            vec!["b\n", "c\n"],
+            vec!["b", "c"],
             lazy_lines.lines(1..3).collect::<Vec<&str>>()
         );
         assert_eq!(
-            vec!["b\n", "c\n", "d\n"],
+            vec!["b", "c", "d"],
             lazy_lines.lines(1..).collect::<Vec<&str>>()
         );
         assert_eq!(
-            vec!["c\n", "b\n"],
+            vec!["c", "b"],
             lazy_lines.lines_reversed(1..3).collect::<Vec<&str>>()
         );
         assert_eq!(
-            vec!["d\n", "c\n", "b\n"],
+            vec!["d", "c", "b"],
             lazy_lines.lines_reversed(1..).collect::<Vec<&str>>()
         );
     }
 
     #[test]
-    fn line_indices() {
-        let lines = LazyLines::from("a\nb\nc\nd\na\nb\nc\nd\n");
-        let indices = lines.get_line_indices();
-        assert_eq!(indices.get("b\n"), Some(&vec![1usize, 5usize]));
-        assert_eq!(indices.get("f\n"), None);
+    fn lazy_lines_no_final_eol() {
+        let lazy_lines: LazyLines = String::from("a\nb\nc\nd").into();
+        assert!(!lazy_lines.has_final_eol());
+        assert_eq!(lazy_lines.eol(), "\n");
+        assert_eq!(lazy_lines.len(), 4);
+        assert_eq!(
+            vec!["b", "c"],
+            lazy_lines.lines(1..3).collect::<Vec<&str>>()
+        );
+        assert_eq!(
+            vec!["b", "c", "d"],
+            lazy_lines.lines(1..).collect::<Vec<&str>>()
+        );
+        assert_eq!(
+            vec!["c", "b"],
+            lazy_lines.lines_reversed(1..3).collect::<Vec<&str>>()
+        );
+        assert_eq!(
+            vec!["d", "c", "b"],
+            lazy_lines.lines_reversed(1..).collect::<Vec<&str>>()
+        );
+    }
+
+    #[test]
+    fn lazy_lines_ms_eol() {
+        let lazy_lines: LazyLines = String::from("a\r\nb\r\nc\r\nd\r\n").into();
+        assert!(lazy_lines.has_final_eol());
+        assert_eq!(lazy_lines.eol(), "\r\n");
+        assert_eq!(lazy_lines.len(), 4);
+        assert_eq!(
+            vec!["b", "c"],
+            lazy_lines.lines(1..3).collect::<Vec<&str>>()
+        );
+        assert_eq!(
+            vec!["b", "c", "d"],
+            lazy_lines.lines(1..).collect::<Vec<&str>>()
+        );
+        assert_eq!(
+            vec!["c", "b"],
+            lazy_lines.lines_reversed(1..3).collect::<Vec<&str>>()
+        );
+        assert_eq!(
+            vec!["d", "c", "b"],
+            lazy_lines.lines_reversed(1..).collect::<Vec<&str>>()
+        );
     }
 }
