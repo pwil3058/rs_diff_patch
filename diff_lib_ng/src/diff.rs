59209510ac16780fcb11d34a176c3235a5d06388
Peter Williams <pwil3058@bigpond.net.au>
2024-05-06 10:03:07 +1000

    Implement Diff

--- diff_lib_ng/src/diff.rs
+++ diff_lib_ng/src/diff.rs
@@ -11,7 +11,7 @@ use crate::snippet::{Snippet, SnippetWrite};
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io;
-use std::io::{BufRead, BufReader, Read};
+use std::io::{BufRead, BufReader, ErrorKind, Read};
 use std::path::{Path, PathBuf};
 
 use crate::data::ExtractSnippet;
@@ -439,5 +439,62 @@ impl PathAndLines {
             into.write_all(line.as_bytes())?;
         }
         Ok(())
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Diff {
+    TextChange(TextChangeDiff),
+    TextAdd(PathAndLines),
+    TextRemove(PathAndLines),
+    ByteChange(ByteChangeDiff),
+    ByteAdd(PathAndBytes),
+    ByteRemove(PathAndBytes),
+}
+
+impl Diff {
+    pub fn new(before_file_path: &Path, after_file_path: &Path, context: u8) -> io::Result<Self> {
+        if before_file_path.exists() {
+            if after_file_path.exists() {
+                match TextChangeDiff::new(before_file_path, after_file_path, context) {
+                    Ok(text_change_diff) => Ok(Self::TextChange(text_change_diff)),
+                    Err(_) => Ok(Self::ByteChange(ByteChangeDiff::new(
+                        before_file_path,
+                        after_file_path,
+                        context,
+                    )?)),
+                }
+            } else {
+                match PathAndLines::new(before_file_path) {
+                    Ok(path_and_lines) => Ok(Self::TextRemove(path_and_lines)),
+                    Err(_) => Ok(Self::ByteRemove(PathAndBytes::new(before_file_path)?)),
+                }
+            }
+        } else if after_file_path.exists() {
+            match PathAndLines::new(after_file_path) {
+                Ok(mut path_and_lines) => {
+                    path_and_lines.path = before_file_path.to_path_buf();
+                    Ok(Self::TextAdd(path_and_lines))
+                }
+                Err(_) => {
+                    let mut path_and_bytes = PathAndBytes::new(after_file_path)?;
+                    path_and_bytes.path = before_file_path.to_path_buf();
+                    Ok(Self::ByteAdd(path_and_bytes))
+                }
+            }
+        } else {
+            Err(io::Error::new(
+                ErrorKind::NotFound,
+                "Neither input file exists!",
+            ))
+        }
+    }
+
+    pub fn from_reader<R: io::Read>(reader: &mut R) -> Result<Self, serde_json::Error> {
+        serde_json::from_reader(reader)
+    }
+
+    pub fn to_writer<W: io::Write>(&self, writer: &mut W) -> Result<(), serde_json::Error> {
+        serde_json::to_writer_pretty(writer, self)
     }
 }
