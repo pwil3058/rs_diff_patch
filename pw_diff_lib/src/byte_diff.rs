1f5f78cc09072c8a50d1e9c6a827ff2c2c29b77c
Peter Williams <pwil3058@bigpond.net.au>
2026-09-20 14:55:27 +1000

    Upgrade to 2024 edition

--- pw_diff_lib/src/byte_diff.rs
+++ pw_diff_lib/src/byte_diff.rs
@@ -42,7 +42,7 @@ impl ByteChangeClump {
     }
 }
 
-impl ApplyClumpClean for ByteChangeClump {
+impl<'a> ApplyClumpClean for ByteChangeClump {
     fn will_apply(&self, data: &Seq<u8>, reverse: bool) -> bool {
         let before = self.before(reverse);
         data.has_subsequence_at(&before.items, before.start)
