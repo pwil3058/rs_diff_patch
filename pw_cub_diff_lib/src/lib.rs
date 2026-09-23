5ffdb62bdc1ed551dd31999909afc0292ad1c5ce
Peter Williams <pwil3058@bigpond.net.au>
2026-09-24 07:07:06 +1000

    remove unused code

--- pw_cub_diff_lib/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2024 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
-
-pub mod text_diff;
-pub mod unified_diff;
-
-pub const TIMESTAMP_RE_STR: &str = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d{9})? [-+]{1}\d{4}";
-pub const ALT_TIMESTAMP_RE_STR: &str =
-    r"[A-Z][a-z]{2} [A-Z][a-z]{2} \d{2} \d{2}:\d{2}:\d{2} \d{4} [-+]{1}\d{4}";
-pub const PATH_RE_STR: &str = r###""([^"]+)"|(\S+)"###;
